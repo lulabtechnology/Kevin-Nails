c66f47d683dbd0595eab49bee8c51044e89ea934
lulabtechnology <131550504+lulabtechnology@users.noreply.github.com>
2025-10-06 20:00:53 -0600

    Create supabaseAdmin.ts

---
+++ apps/web/lib/supabaseAdmin.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const url = process.env.SUPABASE_URL!
+const key = process.env.SUPABASE_SERVICE_ROLE_KEY! // server only
+
+export const supabaseAdmin = createClient(url, key, { auth: { persistSession: false } })
